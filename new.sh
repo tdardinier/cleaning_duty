62e2e9682eb1ec33d6632bfdb053b3b92ad79106
Thibault <t.dardinier@gmail.com>
2020-01-12 01:12:44 +0100

    New name

--- new.sh
+++ new.sh
@@ -1,4 +1,5 @@
 python3 generate.py > html/table.html
 cat html/start.html html/table.html html/end.html > cleaning_duty.html
 google-chrome --headless --screenshot --window-size=1800,800 --default-background-color=0 cleaning_duty.html
-eog screenshot.png
+mv screenshot.png cleaning_duty.png
+eog cleaning_duty.png
