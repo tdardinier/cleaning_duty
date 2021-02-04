7708e20c7c3ee45553da321f209105193a021039
Jacob Gelling <jacobgelling@users.noreply.github.com>
2021-02-04 13:49:05 +0100

    Make Mac compatible

--- new.sh
+++ new.sh
@@ -1,5 +1,10 @@
 python3 generate.py html 9 > html/table.html
 cat html/start.html html/table.html html/end.html > cleaning_duty.html
+
 google-chrome --headless --screenshot --window-size=2000,800 --default-background-color=0 cleaning_duty.html
 mv screenshot.png cleaning_duty.png
-eog cleaning_duty.png
+if [ "$(uname)" == "Darwin" ]; then
+    open -a Preview cleaning_duty.png
+else
+    eog cleaning_duty.png
+fi
