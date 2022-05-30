5340e2023e4fd95348803cb8c682900a275449cf
Jacob Gelling <jacobgelling@users.noreply.github.com>
2022-05-30 23:51:52 +0200

    Regenerate with new seed

---
+++ screenshot.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --headless --screenshot=%cd%\cleaning_duty.png --disable-gpu --window-size=2000,1433 --default-background-color=0 %cd%\cleaning_duty.html
