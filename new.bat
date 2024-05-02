b80b60a319adf05b9a73adb41f4bb1b520b7807f
Jacob Gelling <3254275+jacobgelling@users.noreply.github.com>
2024-05-02 11:41:56 +0200

    Generate new cleaning duty

--- new.bat
+++ new.bat
@@ -1,4 +1,4 @@
 set PYTHONIOENCODING=utf-8
-python generate.py html 21 > html/table.html
+python generate.py html 22 > html/table.html
 wsl cat html/start.html html/table.html html/end.html > cleaning_duty.html
-"C:\Program Files\Google\Chrome\Application\chrome.exe" --headless --screenshot="%cd%\cleaning_duty.png" --disable-gpu --window-size=2000,1433 --default-background-color=0 --hide-scrollbars "%cd%\cleaning_duty.html"
+"C:\Program Files\Google\Chrome\Application\chrome.exe" --headless --screenshot="%cd%\cleaning_duty.png" --disable-gpu --window-size=2000,1433 --default-background-color="ffffff" --hide-scrollbars "%cd%\cleaning_duty.html"
--- new.sh
+++ new.sh
@@ -1,10 +1,10 @@
-python3 generate.py html 21 > html/table.html
+python3 generate.py html 22 > html/table.html
 cat html/start.html html/table.html html/end.html > cleaning_duty.html
 
 if [ "$(uname)" == "Darwin" ]; then
-    /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --screenshot=cleaning_duty.png --window-size=2000,1414 --default-background-color=0 cleaning_duty.html
+    /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --headless --screenshot=cleaning_duty.png --window-size=2000,1414 --default-background-color="ffffff" --hide-scrollbars cleaning_duty.html
     open -a Preview cleaning_duty.png
 else
-    google-chrome --headless --screenshot=cleaning_duty.png  --window-size=2000,1414 --default-background-color=0 --hide-scrollbars cleaning_duty.html
+    google-chrome --headless --screenshot=cleaning_duty.png  --window-size=2000,1414 --default-background-color="ffffff" --hide-scrollbars cleaning_duty.html
     eog cleaning_duty.png
 fi
